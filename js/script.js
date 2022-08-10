38eb37d0c52aefd5149f0067a039264ad31da20b
elizabeth.goullaud <egoullaud@gmail.com>
2022-08-10 12:09:06 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//exercise #1
+var footer = document.querySelector("footer");
+footer.style.backgroundColor = "#D7DDF3";
+console.log(footer);
+
+var h2Tagline = document.querySelector(".tagline");
+h2Tagline.innerText = "Time for you to bloom.";
+console.log(h2Tagline);
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
+console.log(address);
+
+//exercise #2
+var instagramIcon = document.querySelector(".fa-instagram");
+instagramIcon.style.color = "#D45BB9";
+console.log(instagramIcon);
+
+var facebookIcon = document.querySelector(".fa-facebook");
+facebookIcon.style.color = "#6a99b5";
+console.log(facebookIcon);
+
+var footerHours = document.querySelector("footer p");
+footerHours.style.border = "1px solid #d45bb9";
+footerHours.style.padding = "1em";
+console.log(footerHours);
